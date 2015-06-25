d9b95a4b34f3c5290ddad6587a7b4e358e6e5122
richard <rluo0506@gmail.com>
2015-06-24 20:36:29 -0700

    prettify the scrollbar

--- client/templates/chat.js
+++ client/templates/chat.js
@@ -1,50 +1,49 @@
-Template.chat.helpers({
-    groups: function () {
-        return Groups.find();
-    },
-    users: function () {
-        return Meteor.users.find();
-    },
-    heading: function () {
-        var id = Session.get('currentContextId');
-        var currentContext = Groups.findOne(id) || Meteor.users.findOne(id);
-        return currentContext.name || currentContext.username;
+(function () {
+    Template.chat.helpers({
+        groups: function () {
+            return Groups.find();
+        },
+        users: function () {
+            return Meteor.users.find();
+        },
+        heading: function () {
+            return Session.get('currentContext');
+        }
+    });
+
+    Template.chat.events({
+        'submit #input-panel': function (e, t) {
+            e.preventDefault();
+
+            var inputElement = t.find('#input-box');
+            var content = inputElement.value;
+            var context = this;
+
+            //clear inputbox
+            inputElement.value = '';
+            var msg = {
+                context: context.name || context.username,
+                contextId: context._id,
+                content: content,
+                private: assignPrivacy()
+            };
+
+
+            Meteor.call('messageInsert', msg, function (error, result) {
+                if (error) {
+                    console.log(error.reason)
+                } else {
+                    //change to Autorun later
+                    $('#display').scrollTop($('#display')[0].scrollHeight);
+                }
+            })
+        }
+    });
+
+    function assignPrivacy() {
+        var route = Router.current().url;
+        var regex = new RegExp("/private/");
+        var isPrivate = route.match(regex) == '/private/';
+        return isPrivate;
     }
-});
-
-Template.chat.events({
-    'submit #input-panel': function (e, t) {
-        e.preventDefault();
-
-        var inputElement = t.find('#input-box');
-        var content = inputElement.value;
-        var context = this;
-
-        //clear inputbox
-        inputElement.value = '';
-        var msg = {
-            context: context.name || context.username,
-            contextId: context._id,
-            content: content,
-            private: assignPrivacy()
-        };
-
-
-        Meteor.call('messageInsert', msg, function (error, result) {
-            if (error) {
-                console.log(error.reason)
-            } else {
-                //change to Autorun later
-                $('#display').scrollTop($('#display')[0].scrollHeight);
-            }
-        })
-    }
-});
-
-function assignPrivacy() {
-    var route = Router.current().url;
-    console.log(route);
-    var regex = new RegExp("/private/");
-    var isPrivate = route.match(regex) == '/private/';
-    return isPrivate;
-}
+})();
