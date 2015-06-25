31265a2b524b3538a9f978d8b6933e03722a66a5
richard <rluo0506@gmail.com>
2015-06-24 22:35:17 -0700

    switching to master

--- client/templates/signup-form.js
+++ client/templates/signup-form.js
@@ -1,17 +1,30 @@
-Template.signupForm.events({
-    'submit #signup-form': function (e, t) {
-        e.preventDefault();
-        Accounts.createUser({
-            username: t.find('#signup-username').value,
-            password: t.find('#signup-password').value
-        }, function (error) {
-            if (error) {
-                console.log("Account not created. Reason: " + error.reason);
-            }
-        });
-    },
-    'click #back-button': function (e, t) {
-        e.preventDefault();
-        Session.set('clickedSignup', false);
-    }
-});
+(function () {
+    Template.signupForm.events({
+        'submit #signup-form': function (e, t) {
+            e.preventDefault();
+            Accounts.createUser({
+                username: t.find('#signup-username').value,
+                password: t.find('#signup-password').value,
+                //@@@ refactor to global helper. look into meteor template scopes
+                avatar: function () {
+                    var avatars = [
+                        'http://i.imgur.com/tLsEqDo.png',
+                        'http://i.imgur.com/6G0BDmB.png',
+                        'http://i.imgur.com/c0CeFRH.png',
+                        'http://i.imgur.com/ENQREiE.png'
+                    ];
+                    var randomNum = Math.floor(Math.random() * 4);
+                    return avatars[randomNum];
+                }
+            }, function (error) {
+                if (error) {
+                    console.log("Account not created. Reason: " + error.reason);
+                }
+            });
+        },
+        'click #back-button': function (e, t) {
+            e.preventDefault();
+            Session.set('clickedSignup', false);
+        }
+    });
+})();
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,25 +1,39 @@
+(function () {
 //seed groups
-if (!Groups.find().count()) {
-    Groups.insert({
-        name: 'The Oasis'
-    });
+    if (!Groups.find().count()) {
+        Groups.insert({
+            name: 'The Oasis'
+        });
 
-    Groups.insert({
-        name: 'The Desert'
-    });
-}
-
-//seed dummy users
-if (!Meteor.users.find().count()) {
-    for (var i = 1; i <= 5; i++) {
-        Meteor.users.insert({
-            username: 'dummyUser0' + i,
-            "services": {
-                "password": {
-                    "bcrypt": "$2a$10$VUBLmUbT9M2XfCDSMFMIgOfJYOCkWTgKEejUMWkcrJ6o6wBiCcAwW"
-                }
-            },
-            "profile": {}
+        Groups.insert({
+            name: 'The Desert'
         });
     }
-}
+
+//seed dummy users
+    if (!Meteor.users.find().count()) {
+        for (var i = 1; i <= 5; i++) {
+            Meteor.users.insert({
+                username: 'dummyUser0' + i,
+                avatar: assignRandomAvatar(),
+                "services": {
+                    "password": {
+                        "bcrypt": "$2a$10$VUBLmUbT9M2XfCDSMFMIgOfJYOCkWTgKEejUMWkcrJ6o6wBiCcAwW"
+                    }
+                },
+                "profile": {}
+            });
+        }
+    }
+
+    function assignRandomAvatar() {
+        var avatars = [
+            'http://i.imgur.com/tLsEqDo.png',
+            'http://i.imgur.com/6G0BDmB.png',
+            'http://i.imgur.com/c0CeFRH.png',
+            'http://i.imgur.com/ENQREiE.png'
+        ];
+        var randomNum = Math.floor(Math.random() * 4);
+        return avatars[randomNum];
+    }
+})();
